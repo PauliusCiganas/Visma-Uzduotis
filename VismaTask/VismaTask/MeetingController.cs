a941b0f1425880d118d3761eed7b97f9ebc8f034
PauliusCiganas <89936529+PauliusCiganas@users.noreply.github.com>
2022-01-13 21:33:35 +0200

    Navigacija ir failo skaitymas

---
+++ VismaTask/VismaTask/MeetingController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VismaTask
+{
+    public static class MeetingController
+    {
+        public static void Create()
+        {
+            Console.Clear();
+            Console.WriteLine("creating new meeting ...");
+            var testas = new Meeting()
+            {
+                Name = "testName",
+                ResponsiblePerson = "testPerson",
+                Description = "testDescription",
+                Category = Category.Short,
+                Type = Type.InPerson,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now,
+            };
+            DB.Meetings.Add(testas);
+            DB.SaveChanges();
+        }
+
+        public static void Delete()
+        {
+            Console.Clear();
+            Console.WriteLine("delete meeting ...");
+        }
+
+        public static void AddPerson()
+        {
+            Console.Clear();
+            Console.WriteLine("adding person to meeting ..."); 
+        }
+
+        public static void RemovePerson()
+        {
+            Console.Clear();
+            Console.WriteLine("removing person from meeting ...");
+        }
+
+        public static void GetAll() 
+        {
+            Console.Clear();
+            Console.WriteLine("showing all meetings ...");
+        }
+    }
+   
+}
